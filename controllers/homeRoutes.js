965f08205c709974733abd1a9d16c57deff32522
Caleb Wood <Calebwjwood60@gmail.com>
2023-09-20 00:07:48 -0500

    set up the serverjs to render the handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,16 +12,16 @@ router.get("/", async (req, res) => {
         },
       ],
     });
-    console.log(postData);
-    res.status(200).json(postData);
+   
+    
 
     //UNCOMMENT WHEN FRONT END IS READY
-    // const posts = postData.map((post) => post.get({ plain: true }))
+    const posts = postData.map((post) => post.get({ plain: true }))
 
-    // res.render('homepage', {
-    //     posts,
-    //     logged_in: req.session.logged_in
-    // })
+    res.render('homepage', {
+        posts,
+        logged_in: req.session.logged_in
+    })
   } catch (err) {
     res.status(500).json(err);
   }

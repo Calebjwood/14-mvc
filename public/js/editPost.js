a2a1a87c999774423511631b8b3b422475b51158
Caleb Wood <Calebwjwood60@gmail.com>
2023-09-21 01:46:37 -0500

    added the editpost route and js

---
+++ public/js/editPost.js
@@ -0,0 +1,45 @@
+const postId = document.getElementById('postId').value
+const updateBtn = document.getElementById('updateBtn')
+const deleteBtn = document.getElementById('deleteBtn')
+
+
+const updateFormHandler = async (event) => {
+    event.preventDefault()
+
+    const title = document.querySelector('#post-name').value.trim()
+    const postText = document.querySelector('#post-text').value.trim() 
+
+    if(title && postText){
+        const response = await fetch(`/api/post/${postId}`, {
+            method: 'PUT',
+            body: JSON.stringify({ title, postText }),
+            headers: { 'Content-Type': 'application/json' },
+        })
+
+        if (response.ok) {
+            document.location.replace('/dashboard')
+        }else {
+            alert(response.statusText);
+        }
+    }
+}
+
+const deleteFormHandler = async (event) => {
+    event.preventDefault()
+
+    const response = await fetch(`/api/post/${postId}`, {
+        method: 'DELETE',
+    })
+
+    if (response.ok) {
+        document.location.replace('/dashboard')
+    }else {
+        alert(response.statusText);
+    }
+
+}
+
+
+updateBtn.addEventListener('click', updateFormHandler)
+
+deleteBtn.addEventListener('click', deleteFormHandler)
